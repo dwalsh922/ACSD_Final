f1f31acd17254ea19d6fb47c34cf4efc86d8242a
dwalsh922 <116760889+dwalsh922@users.noreply.github.com>
2023-08-01 22:28:25 +0100

    Fix Routing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-        <Router>
+        <Router basename="ACSD_Final">
           <Navbar/>
           <Routes>
             <Route path = "/" element = {<Shop/>}/>
