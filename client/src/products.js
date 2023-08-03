aa3b10beab70998f5e027a4f2404cab6385d0782
dwalsh922 <dav.walsh10@gmail.com>
2023-08-03 14:02:16 +0100

    Resolve Issues

---
+++ client/src/products.js
@@ -0,0 +1,118 @@
+import gloves from "./assets/gloves.png";
+import tshirt from "./assets/t-shirt.webp";
+import hat from "./assets/hat.png";
+import jumper from "./assets/jumper.png";
+import shorts from "./assets/Shorts.png";
+import trousers from "./assets/trousers.png";
+import arsenal from "./assets/My project.png"
+import blueJeans from "./assets/blue-jeans.png"
+import hoodie2 from "./assets/hoodie-2.png"
+import nike from "./assets/nike-hoodie.png"
+import tshirt2 from "./assets/tshirt-2.png"
+import tshirt3 from "./assets/tshirt-3.png"
+import xmas from "./assets/xmas-jumper.png"
+import greenHoodie from "./assets/Green-Hoodie.png"
+import swimShorts from "./assets/swim-shorts.png"
+import cargoPants from "./assets/cargo-pants.png"
+
+export const Products = [
+    {
+        id: 1,
+        productName: "Black T-Shirt",
+        price: 15.00, 
+        productImage: tshirt,
+    },
+    {
+        id: 2,
+        productName: "Grey Sweatshirt",
+        price: 35.00, 
+        productImage: jumper,
+    },
+    {
+        id: 3,
+        productName: "Beanie",
+        price: 10.00, 
+        productImage: hat,
+    },
+    {
+        id: 4,
+        productName: "Beige Trousers",
+        price: 25.00, 
+        productImage: trousers,
+    },
+    {
+        id: 5,
+        productName: "Winter Gloves",
+        price: 5.00, 
+        productImage: gloves,
+    },
+    {
+        id: 6,
+        productName: "Ruby Shorts",
+        price: 20.00, 
+        productImage: shorts,
+    },
+    {
+        id: 7,
+        productName: "Arsenal Football Shorts",
+        price: 30.0, 
+        productImage: arsenal,
+    },
+    {
+        id: 8,
+        productName: "Blue Jeans",
+        price: 40.0, 
+        productImage: blueJeans,
+    },
+    {
+        id: 9,
+        productName: "Brown Oversized Hoodie",
+        price: 37.50, 
+        productImage: hoodie2,
+    },
+    {
+        id: 10,
+        productName: "Nike Hoodie",
+        price: 65.0, 
+        productImage: nike,
+    },
+    {
+        id: 11,
+        productName: "Never Give Up T-Shirt",
+        price: 22.0, 
+        productImage: tshirt2,
+    },
+    {
+        id: 12,
+        productName: "Gucci T-Shirt",
+        price: 25.50, 
+        productImage: tshirt3,
+    },
+    {
+        id: 13,
+        productName: "Christmas Jumper",
+        price: 30.0, 
+        productImage: xmas,
+    },
+    {
+        id: 14,
+        productName: "Green Hoodie",
+        price: 35.0, 
+        productImage: greenHoodie,
+    },
+    {
+        id: 15,
+        productName: "Swim Shorts",
+        price: 15.0, 
+        productImage: swimShorts,
+    },
+    {
+        id: 16,
+        productName: "Cargo Pants",
+        price: 45.0, 
+        productImage: cargoPants,
+    },
+
+
+]
+
